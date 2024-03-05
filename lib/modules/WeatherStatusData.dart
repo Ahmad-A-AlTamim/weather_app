3c4c05e0acbf527f0afc3985e10a8b4f9fbc27c7
Ahmad Tamim <ahmadabedtamim@gmail.com>
2024-03-06 00:02:13 +0200

    weather model + Dio

--- lib/modules/WeatherStatusData.dart
+++ lib/modules/WeatherStatusData.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, non_constant_identifier_names
 
 class WeatherDataStatus {
   String imgUrl, cityName, updatedDate, weatherStatus;
-  int minTemp, maxTemp, avgTemp;
+  double minTemp, maxTemp, avgTemp;
   WeatherDataStatus({
     required this.imgUrl,
     required this.cityName,
@@ -12,4 +12,16 @@ class WeatherDataStatus {
     required this.maxTemp,
     required this.minTemp,
   });
+
+  factory WeatherDataStatus.fromJsom(Json, String imagePath) {
+    return WeatherDataStatus(
+      imgUrl: imagePath,
+      cityName: Json["location"]["name"],
+      updatedDate: Json["current"]["last_updated"],
+      weatherStatus: Json["current"]["condition"]["text"],
+      avgTemp: Json["forecast"]["forecastday"][0]["day"]["avgtemp_c"],
+      maxTemp: Json["forecast"]["forecastday"][0]["day"]["maxtemp_c"],
+      minTemp: Json["forecast"]["forecastday"][0]["day"]["mintemp_c"],
+    );
+  }
 }
